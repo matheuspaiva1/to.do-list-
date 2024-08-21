b977156792bc8945f1d5fbfeb43e3166cae8d139
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-08-20 22:28:32 -0300

    feat: add nova tarefa à pagina inicial

--- scripts.js
+++ scripts.js
@@ -22,6 +22,10 @@ const nomeTask = document.getElementById('nome-task')
 
 const descTask = document.getElementById('desc-task')
 
+const taskCheck = document.getElementById('task-check')
+
+const formTasks = document.getElementById('form-tasks')
+
 addTaskBtn.addEventListener('click', () => {
   modal.style.display = 'flex';
 })
@@ -34,26 +38,132 @@ close.addEventListener('click', () => {
   modal.style.display = 'none'
 })
 
+formTasks.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  let valorInput = inputModal.value
+  let valorTextarea = textareaModal.value
+
+  const novaTarefa = {
+    valorInput: valorInput,
+    valorTextarea: valorTextarea
+  };
+
+  let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+  tarefas.push(novaTarefa);
+
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+  formTasks.reset()
+
+  displayTasks()
+
+})
+
+function displayTasks(){
+  let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+  
+
+  tarefas.forEach(function(tarefa) {
+    if (tarefas){
+
+const taskCheck = document.createElement('div');
+taskCheck.className = 'tasks';
+taskCheck.id = 'task-check';
+
+const checkboxDiv = document.createElement('div');
+checkboxDiv.className = 'checkbox';
+
+const checkboxInput = document.createElement('input');
+checkboxInput.type = 'checkbox';
+
+checkboxDiv.appendChild(checkboxInput);
+
+const descriptionTaskDiv = document.createElement('div');
+descriptionTaskDiv.className = 'description-task';
+descriptionTaskDiv.id = 'task-desc';
+
+const p = document.createElement('p');
+p.textContent = `${tarefa.valorInput}`;
+
+const span = document.createElement('span');
+span.textContent = `${tarefa.valorTextarea}`;
+
+descriptionTaskDiv.appendChild(p);
+descriptionTaskDiv.appendChild(span);
+
+const nullDiv = document.createElement('div');
+nullDiv.className = 'null';
+
+const editAndRemoveDiv = document.createElement('div');
+editAndRemoveDiv.className = 'editAndRemove-tasks';
+
+const editButton = document.createElement('button');
+const editImg = document.createElement('img');
+editImg.src = '/assets/edit-task.svg';
+editImg.alt = 'Editar tarefas';
+editImg.id = 'edit-task';
+editButton.appendChild(editImg);
+
+const removeButton = document.createElement('button');
+const removeImg = document.createElement('img');
+removeImg.src = '/assets/remove-task.svg';
+removeImg.alt = 'Remover tarefas';
+removeImg.id = 'remove-task';
+removeButton.appendChild(removeImg);
+
+editAndRemoveDiv.appendChild(editButton);
+editAndRemoveDiv.appendChild(removeButton);
+
+taskCheck.appendChild(checkboxDiv);
+taskCheck.appendChild(descriptionTaskDiv);
+taskCheck.appendChild(nullDiv);
+taskCheck.appendChild(editAndRemoveDiv);
+
+appearTasks.appendChild(taskCheck);
+
+noTasks.style.display = 'none'
+appearTasks.style.display = 'block'
+    }
+  }
+);
+}
+
+displayTasks()
+
+
+/*
 function valorModal(){
+
   let valorInput = inputModal.value
   let valorTextarea = textareaModal.value
 
   localStorage.setItem('valor1', valorInput)
   localStorage.setItem('valor2', valorTextarea)
 
-  let recValorInput = localStorage.getItem('valor1')
-  let recValorTextarea = localStorage.getItem('valor2')
+  const tarefa = {
+    valorInput: valorInput,
+    valorTextarea: valorTextarea
+  };
 
-  if (recValorInput || recValorTextarea){
+  const tarefaArray = tarefa;
+
+  let tarefas1 = JSON.parse(localStorage.getItem(valorInput)) || [];
+  let tarefas2 = JSON.parse(localStorage.getItem(valorTextarea)) || [];
+
+  tarefas1.push(tarefaArray);
+
+  console.log(tarefaArray);
+
+  if (tarefaArray.valorInput || tarefaArray.valorTextarea){
     taskDesc.innerHTML = `
-      <p>${recValorInput}</p>
-      <span>${recValorTextarea}</span>
+      <p>${tarefaArray.valorInput}</p>
+      <span>${tarefaArray.valorTextarea}</span>
     `
-    noTasks.style.display = 'none'
-    appearTasks.style.display = 'block'
+   
   }
 
-
-
 }
-
+*/
